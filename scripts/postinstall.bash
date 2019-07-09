b4105591c5192d11651bc13de91be70b8cf4703b
Erik Pearson <eapearson@lbl.gov>
2019-07-09 11:33:02 -0700

    switch to yarn

--- scripts/postinstall.bash
+++ scripts/postinstall.bash
@@ -1,11 +1,11 @@
 echo "Running plugin post install script"
 cd react-app && \
-npm install && \
+yarn install && \
 echo "✓ dependencies installed successfully" && \
-npm run build && \
+yarn run build && \
 echo "✓ built successfully" && \
-npm run test -- --watchAll=false && \
+yarn run test -- --watchAll=false && \
 echo "✓ tests run successfully" && \
-npm run install-plugin && \
+yarn run install-plugin && \
 echo "✓ plugin setup successfully" && \
 echo "✓ plugin installed successfully"
